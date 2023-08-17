197bc8d78f00426b97c41c379859e795751d6823
chicostuhl <chicostuhl@gmail.com>
2023-08-17 11:18:10 -0300

    feat: Criando classe Conta Terminal para armazenar os dados do cliente

---
+++ src/main/java/org/example/app/ContaTerminal.java
@@ -0,0 +1,13 @@
+package org.example.app;
+
+import java.math.BigDecimal;
+
+public class ContaTerminal
+{
+    public int numero;
+    public String agencia;
+    public String nomeCliente;
+    public float saldo;
+    public ContaTerminal(){
+    }
+}
